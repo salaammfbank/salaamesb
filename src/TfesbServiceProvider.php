7d2a6243a80fa064c7a1d723796c3eadab4ebfe1
noor farooqy <noorfarooqy@noors-MacBook-Air.local>
2023-09-19 16:37:36 +0400

    rename service provider

--- src/TfesbServiceProvider.php
+++ src/TfesbServiceProvider.php
@@ -4,18 +4,16 @@ namespace Noorfarooqy\Tfesb;
 
 use Illuminate\Support\ServiceProvider;
 
-class NoorfarooqyTfesbServiceProvider extends ServiceProvider
+class TfesbServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         $this->publishes([
             __DIR__ . '/../config/tfesb.php' => config_path('tfesb.php'),
         ], 'tfesb.config');
-
     }
 
     public function register()
     {
-
     }
 }
